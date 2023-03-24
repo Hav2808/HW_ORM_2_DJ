e30704b5d9095884330d3ce9df053ad2d879f777
Alexandr Khoroshkeye <hav28081979@gmail.com>
2023-03-24 15:29:12 +0300

    Homework

---
+++ DJ_orm_HW2/school/views.py
@@ -0,0 +1,15 @@
+# from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student, Teacher
+
+def students_list(request):
+    ordering = 'group'
+    template = 'school/students_list.html'
+    student = Student.objects.all().prefetch_related('teacher')
+    object_list = student.order_by(ordering)
+    context = {'object_list': object_list}
+#     # используйте этот параметр для упорядочивания результатов
+#     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+#
+    return render(request, template, context)
